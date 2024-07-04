e924168306b155f3f8fa8f7ac2e794e995c3863e
Ashish Vasant Vade <115925803+ashishvade@users.noreply.github.com>
2024-07-04 18:29:51 +0530

    changes done in ticket book

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -11,57 +11,87 @@ class clsBook {
         try {
 
             let {
-                seatNo,
+                seatNos,
                 userId,
                 movieId,
-                // isPayment,
                 showtime,
                 screenNo
             } = req.body;
 
-            let seatId = "Seat_" + seatNo + screenNo;
-            const checkSeat = await seat.findOne({ seatId: { $in: seatId }, isBooked: true });
+            let response = [];
 
-            if (checkSeat) {
-                return apiResponse.alreadyExist(res, "Sorry seat ia unavailable", "", "", "");
-            } else {
+            let allSeatsAvailable = true;
+            
+             
+            for (let i = 0; i < seatNos.length; i++) {
+                let seatNo = seatNos[i];
+                let seatId = "Seat_" + seatNo + screenNo;
 
-                let seatAdd = new seat({
-                    seatId: seatId,
-                    seatNo,
-                    movieId,
-                    screenNo,
-                    showtime
-                });
-                console.log("seatAdded", seatAdd)
-                let seatRes = await seatAdd.save();
+                const checkSeat = await seat.findOne({ seatId: seatId, isBooked: true });
 
-                const seatBook = await seat.updateMany({ seatId: { $in: seatId } }, { isBooked: true })
+                if (checkSeat) {
+                    allSeatsAvailable = false;
+                    response.push({ seatNo: seatNo, message: `Sorry, seat ${seatNo} is already booked.` });
+                   
+                }else{
 
-
-                const movieData = await movieModel.findOne()
-
-
-                let ticketId = "Tickt_" + seatNo + new Date().getTime();
-
-
-                let ticketPost = new ticketModel({
-                    ticketId: ticketId,
-                    seatNo,
-                    userId,
-                    movieId,
-                    totalPrice: movieData.moviePrice,
-                    isPayment: true,
-                    showtime,
-                    screenNo
-                });
-
-                let ticketRes = await ticketPost.save();
-
-                return apiResponse.successResponse(res, "ticket created successfully", ticketRes, "", "");
-
+                }
             }
 
+            if (!allSeatsAvailable) {
+
+                return apiResponse.alreadyExist(res, "Some seats are not available", "", "", response);
+            }
+
+            for (let i = 0; i <seatNos.length; i++) {
+
+                let seatNo=seatNos[i];
+
+                let seatId = "Seat_" + seatNo + screenNo;
+                const checkSeat = await seat.findOne({  seatId:seatId , isBooked: true });
+
+                if (checkSeat) {
+                    return apiResponse.alreadyExist(res, `Sorry seat no ${seatNo} is not unavailable  `, "", "", "");
+                } else {
+
+                    let seatAdd = new seat({
+                        seatId: seatId,
+                        seatNo,
+                        movieId,
+                        screenNo,
+                        showtime
+                    });
+                    console.log("seatAdded", seatAdd)
+                    let seatRes = await seatAdd.save();
+
+                    const seatBook = await seat.updateMany({ seatId: { $in: seatId } }, { isBooked: true })
+
+
+                    const movieData = await movieModel.findOne()
+
+
+                    
+                    let ticketId = "Tickt_" + seatNos + new Date().getTime();
+
+                    let ticketPost = new ticketModel({
+                        ticketId: ticketId,
+                        seatNo,
+                        userId,
+                        movieId,
+                        totalPrice: movieData.moviePrice,
+                        isPayment: true,
+                        showtime,
+                        screenNo
+                    });
+
+                    let ticketRes = await ticketPost.save();
+
+                    response.push({ seatNo: seatNo, message: "Ticket for Seat " + seatNo + " created successfully.",ticketRes });
+
+                }
+            }
+          
+            return apiResponse.successResponse(res, "ticket created successfully", response, "", "");
         } catch (error) {
 
             console.log(error);
