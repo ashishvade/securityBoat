84e87ad1b012dc4fe47ecb9f312247bcbfcad99f
Ashish Vasant Vade <115925803+ashishvade@users.noreply.github.com>
2024-07-09 16:32:59 +0530

    changes in cors

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(cors());
 
 app.use(cors({
-  origin: ['https://ashishmoviebook.netlify.app',"http://localhost:3000","http://43.205.140.194" ],// or your specific origin
+  origin: ['https://kairosmovies.netlify.app',"http://localhost:3000","http://43.205.140.194" ],// or your specific origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
@@ -91,18 +91,18 @@ app.use(function (req, res, next) {
   })
 })
 
-// catch 404 and forward to error handler
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function (err, req, res) {
-  // set locals, only providing error in development
+  
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+ 
   res.status(err.status || 500);
   res.render('error');
 });
